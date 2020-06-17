d73ba7746715e50e68f00c001197d59711664061
Aaron Kitzmiller <aaron.kitzmiller@gmail.com>
2020-06-17 19:19:52 -0400

    Update vue.config.js

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,5 +2,5 @@ module.exports = {
 };
 
 if (process.env.NODE_ENV !== 'development') {
-    module.exports.publicPath = '{{project_name}}/static/'
+    module.exports.publicPath = '/{{project_name}}/static/'
 }
